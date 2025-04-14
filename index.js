b1ecb409f1313d299a6e4d76e4aa03496df18bb4
sgudha <harshshrivastav46@gmail.com>
2025-04-14 18:59:09 +0530

    Your commit message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
 
 const { createProduct } = require('./controller/Product');
 const productsRouter = require('./routes/Products');
@@ -12,30 +12,31 @@ const authRouter = require('./routes/Auth');
 const cartRouter = require('./routes/Cart');
 const ordersRouter = require('./routes/Orders');
 
-
 //middlewares
-
 server.use(cors({
-    exposedHeaders:['X-Total-Count']
-}))
+    exposedHeaders: ['X-Total-Count']
+}));
 server.use(express.json()); // to parse req.body
+
 server.use('/products', productsRouter.router);
-server.use('/categories', categoriesRouter.router)
-server.use('/brands', brandsRouter.router)
-server.use('/users', usersRouter.router)
-server.use('/auth', authRouter.router)
-server.use('/cart', cartRouter.router)
-server.use('/Orders', ordersRouter.router)
+server.use('/categories', categoriesRouter.router);
+server.use('/brands', brandsRouter.router);
+server.use('/users', usersRouter.router);
+server.use('/auth', authRouter.router);
+server.use('/cart', cartRouter.router);
+server.use('/Orders', ordersRouter.router);
 
-main().catch(err=> console.log(err));
+main().catch(err => console.log(err));
 
-async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
-    console.log('database connected')
+async function main() {
+    await mongoose.connect('mongodb+srv://Harsh123:Harsh123@cluster0.xzwalgq.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0');
+    console.log('database connected');
 }
-server.get('/',(req, res)=>{
-    res.json({status:'success'})
-})
-server.listen(8080, ()=>{
-    console.log('server started')
-})
+
+server.get('/', (req, res) => {
+    res.json({ status: 'success' });
+});
+
+server.listen(8080, () => {
+    console.log('server started');
+});
